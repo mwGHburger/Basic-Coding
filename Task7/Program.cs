cecda64530459064ca91551d7e7af0c294f708e2
Michael Wang <michael.wang@myob.com>
2020-08-24 14:27:05 +1000

    Completed task 7

--- Task7/Program.cs
+++ Task7/Program.cs
@@ -6,7 +6,16 @@ namespace Task7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Printing multiplication table for numbers up to 12: \n");
+            
+            for(int firstNumber = 1; firstNumber <= 12; firstNumber++)
+            {
+                for(int secondNumber = 1; secondNumber <= 12; secondNumber++)
+                {
+                    Console.WriteLine($"{firstNumber} x {secondNumber} = {firstNumber * secondNumber}");
+                }
+                Console.Write("\n");
+            }
         }
     }
 }
