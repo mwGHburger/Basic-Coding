70c84a535b6e3ffaff3c76c303667079333d75b9
Michael Wang <michael.wang@myob.com>
2020-08-24 15:39:53 +1000

    Fixed embedded git repository issue

---
+++ Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your name? ");
+            string name = Console.ReadLine();
+            if (name == "Alice" || name == "Bob")
+            {
+                Console.WriteLine($"Hey {name}!");
+            }
+            else
+            {
+                Console.WriteLine("Go away!");
+            }
+        }
+    }
+}
