14dff13cab45ecf02b3ccdab12b0971c98db3242
Michael Wang <michael.wang@myob.com>
2020-08-24 13:40:57 +1000

    Completed task 4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -6,7 +6,14 @@ namespace Task4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Please provide a number: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            int total = 0;
+            for(int number = 1; number <= input; number++ )
+            {
+                total += number;
+            }
+            Console.WriteLine($"The sum of 1 to {input} is {total}.");
         }
     }
 }
