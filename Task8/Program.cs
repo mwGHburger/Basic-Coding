b18aec37e7998ba2cccf632ce45250f7de30bd03
Michael Wang <michael.wang@myob.com>
2020-08-24 14:48:34 +1000

    Completed task 8

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -6,7 +6,29 @@ namespace Task8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random rnd = new Random();
+            Console.WriteLine("I'm thinking of a number, can you guess it? ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            int secret = rnd.Next(1,100);
+            int count = 1;
+            while( input != secret) {
+                if (input > secret)
+                {
+                    Console.WriteLine("Your number is larger than what I'm thinking of...");
+                }
+                else
+                {
+                    Console.WriteLine("Your number is smaller than what I'm thinking of...");
+                }
+                Console.Write("Try guessing again: ");
+                int previousGuess = input;
+                input = Convert.ToInt32(Console.ReadLine());
+                if (input != previousGuess)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"Correct! You guess that correct number and it took you {count} tries.");
         }
     }
 }
