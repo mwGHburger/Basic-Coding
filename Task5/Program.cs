6399863bc69a6f2bcb29b80673efba83d20ec4d6
Michael Wang <michael.wang@myob.com>
2020-08-24 13:54:41 +1000

    Completed task 5

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -6,7 +6,19 @@ namespace Task5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Please provide a number: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            int total = 0;
+            for(int number = 1; number <= input; number++)
+            {
+                if (number % 3 == 0 || number % 5 == 0)
+                {
+                    total += number;
+                    Console.Write($"{number}, ");
+                }
+            }
+            Console.WriteLine($"\nThe sum of the numbers 1 to {input}, where the numbers are a multiple of 3 or 5 is {total}.");
+
         }
     }
 }
