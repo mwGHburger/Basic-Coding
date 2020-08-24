13c7877b16053bae3c9c11a79f99c22e2572d00b
Michael Wang <michael.wang@myob.com>
2020-08-24 14:06:38 +1000

    Completed task 6

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -6,7 +6,32 @@ namespace Task6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Please provide a number: ");
+            int numInput = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"Do you want the sum or the product of the number 1 to {numInput}? Type either sum or product: ");
+            string decision = Console.ReadLine();
+            int total = 0;
+            if(decision == "sum")
+            {
+                for(int number = 1; number <= numInput; number++)
+                {
+                    total += number;
+                }
+            }
+            else if (decision == "product")
+            {
+                total = 1;
+                for(int number = 1; number <= numInput; number++)
+                {
+                    total *= number;
+                }
+            }
+            else
+            {
+                Console.WriteLine("You have entered an invalid input!");
+                return;
+            }
+            Console.WriteLine($"The total is {total}.");
         }
     }
 }
